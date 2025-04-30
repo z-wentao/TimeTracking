6b8dc3c993fe8e8aaf18a12a36f5cd314a96a43d
z-wentao <1728679818@qq.com>
2025-04-30 10:34:17 +0800

    Apply template

--- main.go
+++ main.go
@@ -2,25 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1> This website helps me tracking my time and self-reflection daily.")
+	tpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func LogHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>April 30 Time Log</h1>")
 	fmt.Fprintf(w, "8.00AM Morning Routine<br>")
 	fmt.Fprintf(w, "9.00AM Interleaving\n")
+
+	tpl, err := template.ParseFiles("templates/log.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func ReflectionHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Self-Reflection based on daily Log</h1>")
 	w.Header().Set("Content-Type", "text/hmtl; charset=utf-8")
 	fmt.Fprintf(w, "It works well")
+
+	tpl, err := template.ParseFiles("templates/reflection.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 type Router struct{}
