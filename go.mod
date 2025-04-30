7a9d0134af3f7cd9c79e841666893b858a8fc44e
z-wentao <1728679818@qq.com>
2025-04-30 10:07:43 +0800

    Use Chi Framework

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/z-wentao/TimeTracking
 
 go 1.24.2
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +13,8 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func LogHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>April 30 Time Log</h1>")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "8.00AM Morning Routine")
+	fmt.Fprintf(w, "8.00AM Morning Routine<br>")
+	fmt.Fprintf(w, "9.00AM Interleaving\n")
 }
 
 func ReflectionHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,21 +25,14 @@ func ReflectionHandler(w http.ResponseWriter, r *http.Request) {
 
 type Router struct{}
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		HomeHandler(w, r)
-	case "/log":
-		LogHandler(w, r)
-	case "/reflection":
-		ReflectionHandler(w, r)
-	default:
-		http.Error(w, "oh, we cannot found this page", http.StatusNotFound)
-	}
-}
-
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", HomeHandler)
+	r.Get("/log", LogHandler)
+	r.Get("/reflection", ReflectionHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "oh, we cannot found this page", http.StatusNotFound)
+	})
 	fmt.Println("starting the server on 3001...")
-	http.ListenAndServe(":3001", router)
+	http.ListenAndServe(":3001", r)
 }
